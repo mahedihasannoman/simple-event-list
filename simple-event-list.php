03f0815f709205f6a11131b9275cec52dba493cb
mahedihasannoman <mahedihasannoman@gmail.com>
2022-07-28 02:40:40 +0600

    Configuring initial plugin

---
+++ simple-event-list.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     Simple Event List
+ * Plugin URI:      https://exmaple.com
+ * Description:     A Very simple event listing plugin for WordPress.
+ * Author:          Md. Mahedi Hasan
+ * Author URI:      https://www.linkedin.com/in/mahedihasannoman/
+ * Text Domain:     simple-event-list
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Simple_Event_List
+ */
+
+// Your code starts here.
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
