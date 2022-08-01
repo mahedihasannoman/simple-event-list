680b11f08a2d09ed432f93bd7c8bb56f9c717f05
mahedihasannoman <mahedihasannoman@gmail.com>
2022-08-02 00:14:12 +0600

    Code Refactored

--- includes/class-cli-import-events.php
+++ includes/class-cli-import-events.php
@@ -27,57 +27,67 @@ class CLI_Import_Events {
 	 * @return void
 	 */
 	public function import() {
-		$source = SIMPLE_EVENT_LIST_ABSPATH . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'events.json';
-		if ( file_exists( $source ) ) {
+		$source_path = SIMPLE_EVENT_LIST_ABSPATH . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'events.json';
+		$source_url  = SIMPLE_EVENT_LIST_PLUGIN_URL . 'data/events.json';
+		if ( file_exists( $source_path ) ) {
+			try {
+				$response = wp_remote_get( $source_url );
 
-			// Check if file_get_contents is available.
-			if ( function_exists( 'file_get_contents' ) ) {
-
-				$events = file_get_contents( $source );
-				$events = json_decode( $events, true );
-
-				$inserted = 0;
-				$updated  = 0;
-				$failed   = 0;
-				foreach ( $events as $event ) {
-
-					// Check if event is already exists.
-					$post_id = sel_get_post_id( $event['id'] );
-					if ( ! is_null( $post_id ) ) {
-						// Update the event.
-						$result = sel_update_event( $post_id, $event );
-						if ( $result ) {
-							$updated++;
-							// translators: %d: Event ID.
-							\WP_CLI::log( sprintf( esc_html__( 'Event ID: %d updated!', 'simple-event-list' ), (int) $event['id'] ) );
-						} else {
-							$failed++;
-							// translators: %d: Event ID.
-							\WP_CLI::log( sprintf( esc_html__( 'Event ID: %d update failed.', 'simple-event-list' ), (int) $event['id'] ) );
-						}
-					} else {
-						// Insert event.
-						$result = sel_insert_event( $event );
-						if ( $result ) {
-							$inserted++;
-							// translators: %d: Event ID.
-							\WP_CLI::log( sprintf( esc_html__( 'Event ID: %d inserted!', 'simple-event-list' ), (int) $event['id'] ) );
-						} else {
-							$failed++;
-							// translators: %d: Event ID.
-							\WP_CLI::log( sprintf( esc_html__( 'Event ID: %d insert failed.', 'simple-event-list' ), (int) $event['id'] ) );
-						}
-					}
+				if ( is_wp_error( $response ) ) {
+					\WP_CLI::error( $response->get_error_message(), $exit = true );
 				}
 
-				// Send email notification.
-				sel_send_import_notification( $inserted, $updated, $failed, count( $events ) );
+				if ( 200 === wp_remote_retrieve_response_code( $response ) ) {
+					$events = json_decode( $response['body'], true );
+					if ( json_last_error() === JSON_ERROR_NONE ) {
+						$inserted = 0;
+						$updated  = 0;
+						$failed   = 0;
+						foreach ( $events as $event ) {
 
-				// translators: %d: event inserted or updated or failed.
-				\WP_CLI::success( sprintf( esc_html__( '%1$d event(s) are inserted, %2$d event(s) are updated, and %3$d event(s) failed to insert or update!', 'simple-event-list' ), $inserted, $updated, $failed ) );
+							// Check if event is already exists.
+							$post_id = sel_get_post_id( $event['id'] );
+							if ( ! is_null( $post_id ) ) {
+								// Update the event.
+								$result = sel_update_event( $post_id, $event );
+								if ( $result ) {
+									$updated++;
+									// translators: %d: Event ID.
+									\WP_CLI::log( sprintf( esc_html__( 'Event ID: %d updated!', 'simple-event-list' ), (int) $event['id'] ) );
+								} else {
+									$failed++;
+									// translators: %d: Event ID.
+									\WP_CLI::log( sprintf( esc_html__( 'Event ID: %d update failed.', 'simple-event-list' ), (int) $event['id'] ) );
+								}
+							} else {
+								// Insert event.
+								$result = sel_insert_event( $event );
+								if ( $result ) {
+									$inserted++;
+									// translators: %d: Event ID.
+									\WP_CLI::log( sprintf( esc_html__( 'Event ID: %d inserted!', 'simple-event-list' ), (int) $event['id'] ) );
+								} else {
+									$failed++;
+									// translators: %d: Event ID.
+									\WP_CLI::log( sprintf( esc_html__( 'Event ID: %d insert failed.', 'simple-event-list' ), (int) $event['id'] ) );
+								}
+							}
+						}
 
-			} else {
-				\WP_CLI::error( esc_html__( 'file_get_contents function does not exists. Unable to import events.', 'simple-event-list' ), $exit = true );
+						// Send email notification.
+						sel_send_import_notification( $inserted, $updated, $failed, count( $events ) );
+
+						// translators: %d: event inserted or updated or failed.
+						\WP_CLI::success( sprintf( esc_html__( '%1$d event(s) are inserted, %2$d event(s) are updated, and %3$d event(s) failed to insert or update!', 'simple-event-list' ), $inserted, $updated, $failed ) );
+
+					} else {
+						\WP_CLI::error( json_last_error_msg(), $exit = true );
+					}
+				} else {
+					\WP_CLI::error( $response['response']['message'], $exit = true );
+				}
+			} catch ( \Exception $ex ) {
+				\WP_CLI::error( $ex->getMessage(), $exit = true );
 			}
 		} else {
 			\WP_CLI::error( esc_html__( 'Source not found. Unable to import events', 'simple-event-list' ), $exit = true );
